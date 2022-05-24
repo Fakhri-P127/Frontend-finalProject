c8b083f1cf2395117f1498e9cf535796d3f8d464
Fakhri-P127 <fakhrita@code.edu.az>
2022-05-24 16:51:22 +0400

    finished product-details.html

--- assets/js/product-details.js
+++ assets/js/product-details.js
@@ -20,3 +20,18 @@ for (let i = 0; i < thumbs.length; i++) {
     document.querySelector(".zoom").style.backgroundImage = `url(${this.src} )`;
   };
 }
+
+const tabs = document.querySelectorAll(".tab");
+tabs[0].classList.add("tab-show"); //default
+const tabButtons = document.querySelectorAll(".tabs >div>ul>li>button");
+
+const showTab = function () {
+  tabs.forEach((tab) => tab.classList.remove("tab-show"));
+  tabButtons.forEach((button) => button.classList.remove("active--tab-link"));
+  const tabContentID = document.querySelector(`#${this.id}-content`);
+  tabContentID.classList.add("tab-show");
+  const tabButtonID = document.querySelector(`#${this.id}`);
+  tabButtonID.classList.add("active--tab-link");
+};
+
+tabButtons.forEach((button) => button.addEventListener("click", showTab));
